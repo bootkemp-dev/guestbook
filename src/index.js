b77b2ee40ac220a826e22bc7ab56d8c764e8355f
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-01-29 21:54:50 +0100

    Create a template route with sample entries

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
 const port = 8000;
+
+const templatesDir = path.join(__dirname, 'templates');
+
+app.set('view engine', 'ejs');
+app.set('views', templatesDir);
+
+app.get('/', (req, res) => {
+  return res.render('index.ejs', {
+    entries: [
+      { text: 'test entry abc' },
+      { text: 'test entry def' },
+      { text: 'xyz' }
+    ]
+  });
+});
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
